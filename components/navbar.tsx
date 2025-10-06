d9e59dbc47c0d94a1ec1157f63b9af097d362d47
donniejr107 <julyanorisdianto@gmail.com>
2025-10-06 20:53:01 +0700

    minor

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -43,7 +43,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <a href="#home" className="text-xl font-bold text-primary font-mono">
-            {"Donnie"}
+            {"</Donnie>"}
           </a>
 
           {/* Desktop Navigation */}
